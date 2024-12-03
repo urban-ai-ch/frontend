18c26b15639981f11730ce85971b76ef627439de
Saimaneesh <yeturu.saimaneesh@gmail.com>
2024-12-03 21:19:35 +0100

    explore button adapts to login state

--- src/components/BigHeroSection.tsx
+++ src/components/BigHeroSection.tsx
@@ -2,8 +2,12 @@
 import ExploreButton from "./ExploreButton"; // Import the reusable button
 import "./BigHeroSection.css";
 import HomePagemap from "../img/HomePagemap6.jpeg";
+import { useAuth } from "../AuthContext";
 
 const BigHeroSection = () => {
+
+  const auth = useAuth();
+
   return (
     <div className="big-hero-section">
       <img
@@ -13,7 +17,7 @@ const BigHeroSection = () => {
       />
       <div className="hero-content">
         <h1>Visualize urban data on the circular economy like never before</h1>
-        <ExploreButton label="Explore" to="/tool" variant="primary" />
+        <ExploreButton label="Explore" to={auth.isAuthenticated ? "/tool" : "/login"} variant="primary" />
       </div>
       <div className="image-source">
         © <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors
